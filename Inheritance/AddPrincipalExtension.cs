fffa159a44eb23e9be394466132523dade58cbfc
Midhushan <ammidhushan@gmail.com>
2020-01-18 08:55:30 +0530

    done

---
+++ Inheritance/AddPrincipalExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAdvance.Inheritance
+{
+    public static class AddPrincipalExtension
+    {
+        public static void AddPrincipal(this CompoundInterest CI, double Amount)
+        {
+            CI.Principle += Amount;
+        }
+    }
+}
---
+++ Inheritance/ICompareInterest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAdvance.Inheritance
+{
+    public interface ICompareInterest
+    {
+        string Identify();
+        double InterestAmount();
+    }
+}

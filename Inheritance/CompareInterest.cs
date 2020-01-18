31d3f523b777a6056a79cd37df86bdd31c37aafe
Midhushan <ammidhushan@gmail.com>
2020-01-18 09:14:38 +0530

    done

--- Inheritance/CompareInterest.cs
+++ Inheritance/CompareInterest.cs
@@ -9,7 +9,17 @@ namespace CSharpAdvance.Inheritance
         public void Compare()
         {
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Comparing Interest Products...");
 
+            Console.WriteLine("{0,-20} {1,10:0,00}", Product1.Indentify(), Product1.InterestAmount());
+            Console.WriteLine();
+            Console.WriteLine("{0,-20} {1,10:0,00}", Product2.Indentify(), Product2.InterestAmount());
+            Console.WriteLine();
+
+            string BestProduct = Product1.InterestAmount() > Product2.InterestAmount() ? Product1.Indentify() : Product2.Indentify();
+
+            Console.WriteLine("{0} is  the best product", BestProduct);
         }
     }
 }
